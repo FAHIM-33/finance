197fac643cabb1f6dc537c62d66cb252ef5b15dd
FAHIM-33 <shahriyerfahim2012@gmail.com>
2023-08-10 15:26:15 +0600

    cached

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+// service-worker.js
+
+const CACHE_NAME = 'offline-cache';
+const OFFLINE_URL = 'offline.html'; // Make sure to provide the path to your offline HTML file
+
+self.addEventListener('install', (event) => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll([OFFLINE_URL]);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
