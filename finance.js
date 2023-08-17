5da7191ac598abcfc75db85da4a945d50c9f41c8
FAHIM-33 <shahriyerfahim2012@gmail.com>
2023-08-17 22:06:03 +0600

    added A

--- finance.js
+++ finance.js
@@ -1,27 +1,49 @@
+function presentValue(){
+    hideAll()
+    document.getElementById('present-value').style.display = 'block';
+}
+function futureValue(){
+    hideAll()
+    document.getElementById('future-value').style.display = 'block';
+    FV();
+}
+
 function FV(){
+    
+    let parent = document.querySelector('.container');
+    parent.innerHTML = '';
     let pv = document.getElementById("pv").value;
     let n = document.getElementById("n").value;
     let i = document.getElementById("i").value;
     let m = document.getElementById("m").value;
-
-    let fv = pv*Math.pow((1+(i/100)),n);
-
-    if(typeof m !== 'string'){
-        console.log("With 'm' value:" );
-        fv = pv*Math.pow((1+(i/(100*m))),n*m);
+    if(m == ''){
+        m = 1;
     }
-    console.log("Future value(FV) is :" +" "+fv);
-    
+    let fv = pv*Math.pow((1+(i/(100*m))),n*m);
+
     let ans = document.createElement('p');
-    ans.textContent = fv;
-
-    let parent = document.querySelector('.container');
+    ans.textContent = 'FV = '+fv;
     parent.appendChild(ans);
-
 }
 
+function A(){
+    let parent = document.querySelector('.containerPV');
+    parent.innerHTML = '';
+    let pv = document.getElementById("pv-a").value;
+    let n = document.getElementById("n-a").value;
+    let i = document.getElementById("i-a").value;
+    let sv = document.getElementById("sv-a").value;
 
-function PV(){
-
+    let A = i*(pv-(sv/Math.pow((1+i),n)))/1-(1/Math.pow((1+i),n));
+    console.log(A);
+    let ans = document.createElement('p');
+    ans.innerText = 'A = '+A;
+    parent.appendChild(ans)
 }
 
+function hideAll(){
+    let all = document.querySelectorAll('section');
+    for (let i =0;i <all.length;i++){
+        document.querySelectorAll('section')[i].style.display = 'none';
+    }
+}
